bd5a4543a6c505395007a523784b589b64410e34
Chris Bolas <tanlaan33@gmail.com>
2021-12-21 13:47:54 -0800

    Update FlightController

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,16 @@
 class FlightsController < ApplicationController
   def index
+    @flight = if request.query_parameters.empty?
+                Flight.new
+              else
+                Flight.new(flight_params)
+              end
   end
+
+  private
+
+  def flight_params
+    params.require(:flight).permit(:departing_airport_id, :arriving_airport_id, :start)
+  end
+
 end
