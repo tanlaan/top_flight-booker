0795ddf5e3460de74ff7959c7ae6ea74a33dd4eb
Chris Bolas <tanlaan33@gmail.com>
2021-12-21 13:40:09 -0800

    Fix indexing for flights

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'flights/index'
+  get '/flights', to: 'flights#index'
   root to: "flights#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
