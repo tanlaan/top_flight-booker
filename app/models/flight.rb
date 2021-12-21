eeb0ec733acc70c7d60342a4da8c5db0f0d5b7ab
Chris Bolas <tanlaan33@gmail.com>
2021-12-21 13:40:32 -0800

    Add validation and samday function to Flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,9 @@
 class Flight < ApplicationRecord
   belongs_to :departing_airport, class_name: 'Airport'
   belongs_to :arriving_airport, class_name: 'Airport'
+  validates :deapring_airport_id, :arriving_airport_id, :start, presence: true
+
+  scope :sameday, lambda { |day|
+    where('date(?) = date(start)', day)
+  }
 end
