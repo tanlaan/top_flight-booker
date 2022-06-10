5c7fad99a46b63c9eec3a15bacd600a38299fc9b
Chris Bolas <tanlaan33@gmail.com>
2022-06-10 11:46:38 -0700

    Add example confirmation email

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -6,6 +6,11 @@ class BookingController < ApplicationController
 
   def create
     @booking = Booking.create(booking_params)
+    
+    # Send confirmation emails
+    for passenger in @booking.passengers do
+      PassengerMailer.confirmation_email(passenger).deliver_later
+    end
     redirect_to @booking
   end
 
--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+    default from: 'notifications@example.com'
+
+    def confirmation_email(passenger)
+        @passenger = passenger
+        @url = 'http://www.example.com'
+        mail(to: @passenger.email, subject: "Please confirm your tickets.")
+    end
 end
